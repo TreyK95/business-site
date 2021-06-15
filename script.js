01722c0ed3ca977be7b3b34cab9280d1dca88ba8
Trey Kemp <kemptrey@outlook.com>
2021-06-15 10:40:43 -0600

    video and controls styled and working

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("change");
 });
 // end of navbar
+
+// section 2 video
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
+
+// end of section 2 video
